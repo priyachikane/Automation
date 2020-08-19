b3a53cd610fe19c114e417d4d0dfd7f23f335798
Om <Om@DESKTOP-KARIHGE>
2020-08-19 11:57:01 -0700

    Automation Git Project

---
+++ AutomationGit/src/main/java/AutomationGit/AutomationGit/App.java
@@ -0,0 +1,13 @@
+package AutomationGit.AutomationGit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
